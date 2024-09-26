7897b785490ce41c0ccd276e84a453bfcc740ded
DeyDeyDream <2677933891@qq.com>
2024-09-26 14:01:31 +0800

    文件目录更改

--- Draft_1/test.cpp
+++ Draft_1/test.cpp
@@ -1,28 +1,42 @@
-#define _CRT_SECURE_NO_WARNINGS
+﻿#define _CRT_SECURE_NO_WARNINGS
 
 #include <iostream>
 using namespace std;
 
-class A {
-public:
-	A() { cout << "A" << endl; }
-};
-class B {
-public:
-	B() { cout << "B" << endl; }
-};
-class C {
-public:
-	C() { cout << "C" << endl; }
-};
-class D {
-public:
-	D() { cout << "D" << endl; }
-};
-C c;
-int main() {
-	A a;
-	B b;
-	static D d;
-	return 0;
-}
+//template<class T1, typename T2>
+
+//template <class T>
+//T fun(T x, T y) {
+//
+//	return x * x + y * y;
+//}
+
+//int main() {
+//	fun<float>(1, 2.0);
+//	fun(1.0, 2);
+//	return 0;
+//}
+
+//class A {
+//public:
+//	A() { cout << "A" << endl; }
+//};
+//class B {
+//public:
+//	B() { cout << "B" << endl; }
+//};
+//class C {
+//public:
+//	C() { cout << "C" << endl; }
+//};
+//class D {
+//public:
+//	D() { cout << "D" << endl; }
+//};
+//C c;
+//int main() {
+//	A a;
+//	B b;
+//	static D d;
+//	return 0;
+//}
