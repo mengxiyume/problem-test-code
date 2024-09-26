072c98bcff611a4b8b3040f541461ebe27ef987b
DeyDeyDream <2677933891@qq.com>
2024-09-26 20:55:55 +0800

    2024/9/26结束了

--- Draft_1/test.cpp
+++ Draft_1/test.cpp
@@ -40,3 +40,33 @@ using namespace std;
 //	static D d;
 //	return 0;
 //}
+
+//int main()
+//{
+//	//挤在一起没有注释的代码太难读了，不整理一下根本看不懂
+//	string strText = "How are you?";
+//	string strSeparator = " ";
+//	string strResult;
+//
+//	int size_pos = 0;
+//	int size_prev_pos = 0;
+//
+//	while ((size_pos = strText.find_first_of(strSeparator, size_pos)) != string::npos) {
+//		strResult = strText.substr(size_prev_pos, size_pos - size_prev_pos);
+//		cout << strResult << " ";
+//		size_prev_pos = ++size_pos;
+//	}
+//
+//	if (size_prev_pos != strText.size()) {
+//		strResult = strText.substr(size_prev_pos, size_pos - size_prev_pos);
+//		cout << strResult << " ";
+//	}
+//	cout << endl;
+//	return 0;
+//
+//}
+
+int main() {
+
+	return 0;
+}
